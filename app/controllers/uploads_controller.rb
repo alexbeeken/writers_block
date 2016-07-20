41a3a0f95cfa6d7375d7cdd4785f363a4bbc3a2d
Alex Beeken <abeeken@focusvision.com>
2016-07-19 21:56:33 -0700

    refactor uploads_controller

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,18 +1,20 @@
 class UploadsController < ApplicationController
   def create
     upload = Upload.create(upload_params)
-    cl_response = ClarifaiRuby::TagRequest.new.get(upload.image.url)
-    words = cl_response.tag_images[0].tags_by_words
-    pg = PhraseGenerator.new(words.join(' '))
     Phrase.create(
       words: words,
-      content: pg.perform,
+      content: PhraseGenerator.new(get_clarifai_tags),
       upload: upload
     )
     redirect_to root_path
   end
 
   private
+
+  def get_clarifai_tags(upload)
+    cl_response = ClarifaiRuby::TagRequest.new.get(upload.image.url)
+    cl_response.tag_images[0].tags_by_words.join(' ')
+  end
 
   def upload_params
     params.require(:upload).permit(:image)
